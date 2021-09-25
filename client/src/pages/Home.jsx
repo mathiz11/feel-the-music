522640253481d356c1cd97c872248d18cba23502
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-09-25 15:06:43 +0200

    add home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "../components/Layout";
+import {Button, Container, Flex, Input} from "@chakra-ui/react";
+import {BsSearch} from "react-icons/all";
+
+export default function Home() {
+    return (
+        <Layout>
+            <Container my={"2rem"}>
+                <Flex>
+                    <Input placeholder={"Musique, Artiste, Paroles..."} value={searchValue}
+                           onChange={(e) => setSearchValue(e.target.value)} mr={"2rem"}/>
+                    <div>
+                        <Button colorScheme={"blue"} leftIcon={<BsSearch/>}>Rechercher</Button>
+                    </div>
+                </Flex>
+            </Container>
+        </Layout>
+    )
+}
