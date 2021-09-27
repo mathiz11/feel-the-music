fdbdfeef071c3cbe4f1783c6a8fd1277ed24d1a0
Mehdi <Mehdi>
2021-09-27 09:00:47 +0200

    [Issue] Récupérer un token d'accès de Genius API

---
+++ api/utils.js
@@ -0,0 +1,19 @@
+const checkAccessToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        if(token){
+            next();
+        }else{
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+module.exports = {
+    checkAccessToken
+}
+
