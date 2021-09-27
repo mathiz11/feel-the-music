ce63b2af590e6fc0ecbfb597836ff85e35366f25
Mehdi <Mehdi>
2021-09-27 09:00:47 +0200

    [Issue] Récupérer un token d'accès de Genius API

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
 const express = require('express')
+const {geniusService} = require("./geniusService");
+const {checkAccessToken} = require("./utils");
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/api/authenticate', async (req, res) => {
+  const accessToken = await geniusService.authenticate();
+  res.send(accessToken);
 })
 
 app.listen(port, () => {
